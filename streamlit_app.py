cd2d2a7baee633fa2e97abeb3271d76a2b2bff51
Stéphane Berhault <144647002+Stekosan@users.noreply.github.com>
2025-10-22 18:27:21 +0900

    Update streamlit_app.py
    add api smoothifroot.com

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,11 @@ if ingredients_list:
 
     time_to_insert = st.button('Submit Order')
 
+  # New section to display smoothiefront nutrition information
+import requests
+smoothiefront_response = request.get("https://my.smoothiefroot.com/api/fruit/watermelon"
+st.text(smoothiefroot_response)
+
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}!', icon="✅")
